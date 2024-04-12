fcefb1be913d69e365f2a25b8c8671a5194ec7a7
Fhyasa <141627135+Fhyasa@users.noreply.github.com>
2024-04-12 12:00:51 +0200

    Add files via upload

---
+++ CSV Script.ps1
@@ -0,0 +1,25 @@
+﻿Import-Module ActiveDirectory
+
+$CSVFilePath = "C:\Users\sysadmin\Desktop\MyCSV.Csv"
+$MyMailDomain = "@hq.sde.gf2"
+
+$Users = Import-Csv $CSVFilePath
+
+Foreach ($User in @Users) {
+New-ADUser -Name ($User.Givename + "." + $User.Surname)
+-SamAccountName $User.Samaccount
+-UserPrincipalName ($User.SamAáccount + $MyMailDomain)
+-Surname $User.Surname
+-GivenName $User.Givenname
+-Path $user.OU
+-AccountPassword (ConvertTo-SecureString $User.Password -AsPlainText -Force)
+-ChangePasswordAtLogon $true
+-Enabled $true
+
+Add-ADGroupMember -Identity "Domain Users" -Members $User.Samaccount
+Add-ADGroupMember - Identity "Remote Desktop Users" -Members $User.Samaccount
+
+$HomeFolderPath = $User.HomeFolder
+New-Item -ItemType Directory -Path $HomeFolderPath
+
+Set-AdUser $User.Samaccount -HomeDrive "H:" -HomeDirectory $HomeFolderPath -ProfilePath $User.profilePath
